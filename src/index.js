31513e5603cf6946365fb9c3484abe79c1d6436f
JudasPriest18 <73815035+JudasPriest18@users.noreply.github.com>
2020-11-30 12:55:06 +1000

    add prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-
-import  './styles/css/style.css'
-import './styles/btn.scss'
-import './styles/header.scss'
-import './styles/main.scss'
-import './styles/container.scss'
-import './styles/fonts.scss'
-import './styles/inputs.scss'
-import './styles/img.scss'
-import './styles/content.scss'
-import './styles/var.scss'
-import './js/wrapper.js'
-
+import "./styles/css/style.css";
+import "./styles/btn.scss";
+import "./styles/header.scss";
+import "./styles/main.scss";
+import "./styles/container.scss";
+import "./styles/fonts.scss";
+import "./styles/inputs.scss";
+import "./styles/img.scss";
+import "./styles/content.scss";
+import "./styles/var.scss";
+import "./js/wrapper.js";
